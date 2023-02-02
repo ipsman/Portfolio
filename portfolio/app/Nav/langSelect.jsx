e95c500345519fd19f7d286e0157fb4d4d35bbcf
ipsman <123635613+ipsman@users.noreply.github.com>
2023-02-02 15:29:18 +0100

    LangUpdate

--- portfolio/app/Nav/langSelect.jsx
+++ portfolio/app/Nav/langSelect.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from "react";
+import { LangSelectOn } from "./navBar";
 
 export default function LangSelect() {
     const languages = [
@@ -16,23 +16,24 @@ export default function LangSelect() {
     const ids = ["English", "Hungarian", "wTitle", "wTitle2", "abTitle", "abTitle2", "btnTitle", "hTitle", "hTitle2", "eTitle", "homeTitle", "endTitle", "nabTitle", "nhTitle"]
     const hu = ["Angol","Magyar","Üdvözöllek","Ez a portfolióm","Rólam", "Máté vagyok, 20 éves pc őrült. Jelenleg számítástechnikát tanulok Budapesten, és szabadidőmben indie játékokon dolgozom.", "Töltsd le a CV-m", "Hogyan készült", "Ez a weboldal React js-el és Tailwind-el készült.", "Köszönöm a figyelmet!", "Kezdőlap", "Vége", "Rólam", "Hogyan készült"]
     const en = ["English","Hungarian","Welcome","This is my portfolio", "About me", "My name is Máté, I'm an 20 year old computer enthusiast. I'm currently studying Computer Science in Budapest, Hungary, and working on indie games in my free time.", "Download my CV", "How It's Made", "This website was made with React js and Tailwind.", "Thanks for your attention!", "Home", "End", "About me","How It's Made"]
-    
-
-    function ChangeLang(num) {
-        if(num == 0){
-            for (let index = 0; index < 14; index++) {
-                document.getElementById(ids[index]).textContent = en[index];   
-            }
-        }
-        else{
-            for (let index = 0; index < 14; index++) {
-                document.getElementById(ids[index]).textContent = hu[index];   
-            }
-        }
-    }
 
     return(
         <ul id="langSelect" className="block scale-0 origin-top-right overflow-hidden fixed z-50 top-11 right-1 shadow-lg backdrop-blur-md p-1 w-36 bg-[#3636363f] rounded-lg gap-2.5 duration-700">{listItems}</ul>
     )
 }
 
+function ChangeLang(num) {
+    if(num == 0){
+        for (let index = 0; index < 14; index++) {
+            document.getElementById(ids[index]).textContent = en[index];   
+        }
+    }
+    else{
+        for (let index = 0; index < 14; index++) {
+            document.getElementById(ids[index]).textContent = hu[index];   
+        }
+    }
+
+    sessionStorage.setItem("langSelectNum", 1);
+    LangSelectOn();
+}
--- portfolio/app/Nav/navBar.jsx
+++ portfolio/app/Nav/navBar.jsx
@@ -5,20 +5,7 @@ import Lang from "app/Icons/lang"
 
 
 export default function NavBar() {
-    var langBool = false;
-    function LangSelectOn() {
-
-        if(langBool){
-            document.getElementById("langSelect").style.transform = "scale(0,0)";
-
-            langBool = false;
-        }
-        else{
-            document.getElementById("langSelect").style.transform = "scale(1,1)";
-
-            langBool = true;
-        }
-    }
+    
 
     return (
            <nav className="fixed z-50 w-screen bg-[#3636363f] shadow-lg backdrop-blur-md p-1 sm:text-lg md:text-xl lg:text-2xl gap-2">
@@ -31,3 +18,20 @@ export default function NavBar() {
            </nav>
 )}
 
+var langBool = 0;
+
+export function LangSelectOn() {
+    langBool = sessionStorage.getItem("langSelectNum");
+
+    if(langBool == 1){
+        document.getElementById("langSelect").style.transform = "scale(0,0)";
+
+        langBool = 0;
+    }
+    else{
+        document.getElementById("langSelect").style.transform = "scale(1,1)";
+
+        langBool = 1;
+    }
+    sessionStorage.setItem("langSelectNum", langBool);
+}
