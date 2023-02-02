5a98f20900b0cd4545e5654704fd7e15d2326bca
ipsman <123635613+ipsman@users.noreply.github.com>
2023-02-02 15:29:58 +0100

    Update end.jsx

--- portfolio/app/Sections/end.jsx
+++ portfolio/app/Sections/end.jsx
@@ -1,6 +1,6 @@
 export default function End() {
     return (
             <div id="End" className="sticky z-40 top-0 h-screen flex flex-col items-center justify-center backdrop-blur-md bg-[#3636363f] text-white gap-2.5 duration-700">
-                <h2 onLoad={renderPage} id="eTitle" className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl">Thanks for your attention!</h2>
+                <h2 id="eTitle" className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl">Thanks for your attention!</h2>
             </div>
 )}
