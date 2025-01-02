135f04b1f7c15a689ecc7fd227ace58d413ddc25
ipsman <fogkefegaming@gmail.com>
2025-01-03 00:42:11 +0100

    update

--- portfolio/app/Sections/howItsMade.jsx
+++ portfolio/app/Sections/howItsMade.jsx
@@ -2,7 +2,7 @@
 
 const HowItsMade = () =>{
         const navigation = [
-        { name: "Fányi klíma", id: 'klima', desc: "A website made in React JS.", src:"https://miro.medium.com/v2/resize:fit:1400/0*OLBBmwZWiKUNk685.png", url: "www.fanyiklima.hu"},
+        { name: "Fányi klíma", id: 'klima', desc: "A website made in React JS.", src:"https://miro.medium.com/v2/resize:fit:1400/0*OLBBmwZWiKUNk685.png", url: "http://www.fanyiklima.hu"},
         { name: "Java project", id: 'java', desc: "Java application for my university project.", src:"https://miro.medium.com/v2/resize:fit:700/0*gtY-llyEbkeoS1Sp.png", url: "https://drive.google.com/file/d/1-niMO32UoZ49Qn6HN5DRH9dZQPm7lc1m/view?usp=sharing"},
         { name: "Python project", id: 'python', desc: "A Python application to track gym membership.", src:"https://innovationspace.ansys.com/wp-content/uploads/2024/03/intro-to-python-1024x683.jpg", url: "https://github.com/ipsman/pythonBeadando"},
         { name: "MyNeptun App", id: 'neptun', desc: "Mobile application for neptun (unofficial).", src:"https://www.macrix.eu/wp-content/uploads/2022/09/Blog-1-1024x452.png", url: ""},
@@ -12,7 +12,7 @@ const HowItsMade = () =>{
         <div id={item.id} key={item.name} className="grid gap-2 bg-[#3636363f] rounded-lg p-4 justify-center content-center shadow-lg hover:scale-105 duration-700">
             <p className="text-3xl">{item.name}</p>
             <p>{item.desc}</p>
-            <a href={item.url} className="px-3">View</a>
+            <a href={item.url} target="_blank" rel="noopener noreferrer" className="px-3">View</a>
         </div>
         
     );
